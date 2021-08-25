65ad96e093bea3305dead65c49cdd7862096409a
غلامرضا بهشتیان <amintado@gmail.com>
2021-08-26 00:53:57 +0430

    add builded packages

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: env('DATABASE_DEFAULT', 'default'),
   connections: {
     default: {
       connector: 'bookshelf',
@@ -16,5 +16,21 @@ module.exports = ({ env }) => ({
         useNullAsDefault: true,
       },
     },
+    test: {
+      connector: 'bookshelf',
+      settings: {
+        client: env('DATABASE_TEST_CLIENT', 'sqlite'),
+        filename: env('DATABASE_TEST_FILE', '.tmp/data.db'),
+        port: env.int('DATABASE_TEST_PORT', 3306),
+        username: env('DATABASE_TEST_USERNAME', 'root'),
+        host:     env('DATABASE_TEST_HOST', 'localhost'),
+        password: env('DATABASE_TEST_PASSWORD', ''),
+        database: env('DATABASE_TEST_NAME', 'pblog'),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+
   },
 });
